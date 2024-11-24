59870412ae23ef4508a18dca4607444105db8ad3
hance0103 <83891076+hance0103@users.noreply.github.com>
2024-11-24 18:41:16 +0900

    점프 수정, 콜라이더 수정

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -26,16 +26,6 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameManager.Input.KeyAction -= OnKeyDown;
-        GameManager.Input.KeyAction += OnKeyDown;
 
-
-    }
-    public void OnKeyDown()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("АјАн");
-        }
     }
 }
