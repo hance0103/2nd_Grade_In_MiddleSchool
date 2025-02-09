c2488f0bb71f851b52c2d8c88fd03678cbadfc00
Doumoman <mp3247472@naver.com>
2025-02-09 17:36:58 +0900

    UI 스프라이트 적용 (2)

--- Assets/Scripts/UIs/PopupUIs/DefeatPopup.cs
+++ Assets/Scripts/UIs/PopupUIs/DefeatPopup.cs
@@ -4,7 +4,23 @@ using TMPro;
 public class DefeatPopup : MonoBehaviour
 {
     [SerializeField]
-    private string[] Defeattexts = {
+    private string[] Defeattexts1 = {
+        "네놈 때문에..오늘도 지각이구나..",
+        "크윽...이 몸이 지다니..치욕스럽구나",
+        "오늘의 패배는..죽을때까지 잊지 않겠다..",
+        "믿을 수 없다..신호등 따위에 당하다니..",
+        "방심했구나..다음 번엔 봐주지 않겠다 "
+    };
+    [SerializeField]
+    private string[] Defeattexts2 = {
+        "네놈 때문에...",
+        "크윽...치욕스럽구나",
+        "오늘의 패배는..잊지 않겠다..",
+        "믿을 수 없다..당하다니..",
+        "방심했구나.."
+    };
+    [SerializeField]
+    private string[] Defeattexts3 = {
         "네놈 때문에..오늘도 지각이구나..",
         "크윽...이 몸이 지다니..치욕스럽구나",
         "오늘의 패배는..죽을때까지 잊지 않겠다..",
@@ -12,27 +28,70 @@ public class DefeatPopup : MonoBehaviour
         "방심했구나..다음 번엔 봐주지 않겠다 "
     };
     [SerializeField] private TMP_Text displayText;
-    public void ShowRandomText()
+    public void ShowRandomText1()
     {
         // 배열 범위 내에서 무작위 인덱스 선택
-        int randomIndex = Random.Range(0, Defeattexts.Length);
+        int randomIndex = Random.Range(0, Defeattexts1.Length);
 
         // 선택된 텍스트를 UI에 표시
         if (displayText != null)
         {
-            displayText.text = Defeattexts[randomIndex];
+            displayText.text = Defeattexts1[randomIndex];
         }
     }
+    public void ShowRandomText2()
+    {
+        // 배열 범위 내에서 무작위 인덱스 선택
+        int randomIndex = Random.Range(0, Defeattexts2.Length);
 
+        // 선택된 텍스트를 UI에 표시
+        if (displayText != null)
+        {
+            displayText.text = Defeattexts2[randomIndex];
+        }
+    }
+    public void ShowRandomText3()
+    {
+        // 배열 범위 내에서 무작위 인덱스 선택
+        int randomIndex = Random.Range(0, Defeattexts3.Length);
+
+        // 선택된 텍스트를 UI에 표시
+        if (displayText != null)
+        {
+            displayText.text = Defeattexts3[randomIndex];
+        }
+    }
     // 패배 시간을 출력하기 위한 텍스트 오브젝트들
     [SerializeField] private TMP_Text displayDefeatTime;
     [SerializeField] private float curTime;
     int minute;
     int second;
     // 패배 팝업 열기 (게임 일시정지)
-    public void OpenDefeat()
+    public void OpenDefeat1()
     {
-        ShowRandomText();
+        ShowRandomText1();
+        gameObject.SetActive(true);
+        Time.timeScale = 0f; // 시간 정지
+        Timer timer = FindObjectOfType<Timer>(); //패배한 시간을 저장하지 않으므로 참조만 하기
+        curTime = timer.curTime;
+        minute = (int)curTime / 60;
+        second = (int)curTime % 60;
+        displayDefeatTime.text = "분투한 시간 : " + minute.ToString("00") + ":" + second.ToString("00");
+    }
+    public void OpenDefeat2()
+    {
+        ShowRandomText2();
+        gameObject.SetActive(true);
+        Time.timeScale = 0f; // 시간 정지
+        Timer timer = FindObjectOfType<Timer>(); //패배한 시간을 저장하지 않으므로 참조만 하기
+        curTime = timer.curTime;
+        minute = (int)curTime / 60;
+        second = (int)curTime % 60;
+        displayDefeatTime.text = "분투한 시간 : " + minute.ToString("00") + ":" + second.ToString("00");
+    }
+    public void OpenDefeat3()
+    {
+        ShowRandomText3();
         gameObject.SetActive(true);
         Time.timeScale = 0f; // 시간 정지
         Timer timer = FindObjectOfType<Timer>(); //패배한 시간을 저장하지 않으므로 참조만 하기
