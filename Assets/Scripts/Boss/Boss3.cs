00e615752c2eb1945493498ef7348ddf4e643ed7
isruiix <korea001yt@gmail.com>
2025-03-27 20:06:44 +0900

    보스3 전용 레이저

--- Assets/Scripts/Boss/Boss3.cs
+++ Assets/Scripts/Boss/Boss3.cs
@@ -119,8 +119,8 @@ public class Boss3 : MonoBehaviour
 
         patternDic.Add(0, new BossState[] {
             //BossState.WeakPattern1,
-            BossState.WeakPattern2,
-           // BossState.WeakPattern3,
+           // BossState.WeakPattern2,
+            BossState.WeakPattern3,
             //BossState.WeakPattern4,
             //BossState.WeakPattern5,
             //BossState.EnragedPattern,
