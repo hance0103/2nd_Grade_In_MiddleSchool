6d82b106870345996551d480ad7fab08ac6bf1a5
isruiix <korea001yt@gmail.com>
2025-05-11 20:16:11 +0900

    죽는 연출 스프라이트 크기 조정

--- Assets/Scripts/Boss/bossPatternTest.cs
+++ Assets/Scripts/Boss/bossPatternTest.cs
@@ -108,10 +108,10 @@ public class bossPatternTest : MonoBehaviour
 
         patternDic.Add(0, new BossState[] {
             BossState.WeakPattern1,
-            //BossState.WeakPattern2,
-            //BossState.WeakPattern3,
-            //BossState.StrongPattern1,
-            //BossState.StrongPattern2
+            BossState.WeakPattern2,
+            BossState.WeakPattern3,
+            BossState.StrongPattern1,
+            BossState.StrongPattern2
         });
         //patternDic.Add(1, new BossState[] { BossState.WeakPattern2, BossState.WeakPattern3, BossState.WeakPattern1 });
         //patternDic.Add(2, new BossState[] { BossState.WeakPattern2, BossState.WeakPattern3, BossState.WeakPattern1 });
