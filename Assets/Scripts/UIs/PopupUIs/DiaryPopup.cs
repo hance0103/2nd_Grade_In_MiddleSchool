a989ca2f20afc152bc20e5afb6d29c1369f6cc8b
Doumoman <mp3247472@naver.com>
2025-06-21 16:50:04 +0900

    다이어리 UI 적용

--- Assets/Scripts/UIs/PopupUIs/DiaryPopup.cs
+++ Assets/Scripts/UIs/PopupUIs/DiaryPopup.cs
@@ -29,11 +29,11 @@ public class DiaryPopup : MonoBehaviour
         Debug.Log(savedTime1);
         minute = (int)curTime / 60;
         second = (int)curTime % 60;
-        Stage1ClearTime.text = "클리어 시간 : " + minute.ToString("00") + ":" + second.ToString("00");
+        Stage1ClearTime.text = minute.ToString("00") + ":" + second.ToString("00");
         
         string SavedText1 = PlayerPrefs.GetString("FinalText1", "아직 도륙내지 않았다!");
         Debug.Log("불러온 데이터: " + SavedText1);
-        Stage1ClearText.text = "피니쉬 대사: " + SavedText1;
+        Stage1ClearText.text = "                      " + SavedText1;
 
     }
     
@@ -48,10 +48,10 @@ public class DiaryPopup : MonoBehaviour
         Debug.Log(savedTime2);
         minute = (int)curTime / 60;
         second = (int)curTime % 60;
-        Stage2ClearTime.text = "클리어 시간 : " + minute.ToString("00") + ":" + second.ToString("00");
+        Stage2ClearTime.text = minute.ToString("00") + ":" + second.ToString("00");
         string SavedText2 = PlayerPrefs.GetString("FinalText2","아직 도륙내지 않았다!");
         Debug.Log("불러온 데이터: " + SavedText2);
-        Stage2ClearText.text = "피니쉬 대사: " + SavedText2;
+        Stage2ClearText.text = "                      " + SavedText2;
 
 
         float savedTime3 = PlayerPrefs.GetFloat("FinalTime3", 0f);
@@ -60,10 +60,10 @@ public class DiaryPopup : MonoBehaviour
         Debug.Log(savedTime3);
         minute = (int)curTime / 60;
         second = (int)curTime % 60;
-        Stage3ClearTime.text = "클리어 시간 : " + minute.ToString("00") + ":" + second.ToString("00");
+        Stage3ClearTime.text = minute.ToString("00") + ":" + second.ToString("00");
         string SavedText3 = PlayerPrefs.GetString("FinalText3", "아직 도륙내지 않았다!");
         Debug.Log("불러온 데이터: " + SavedText3);
-        Stage3ClearText.text = "피니쉬 대사: " + SavedText3;
+        Stage3ClearText.text = "                      " + SavedText3;
 
     }
 
